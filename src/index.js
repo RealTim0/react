5e6a7e5218c22b5c697a86898ef77421d9d06120
RealTim0 <gemtimo994@gmail.com>
2023-06-30 14:40:29 +0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import Table from './table';
- ReactDOM.render(<Table />,document.getElementById('root'));
+import App from './App';
+ ReactDOM.render(<App />,document.getElementById('root'));
 
 
