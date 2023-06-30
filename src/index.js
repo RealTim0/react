80db94101aa3ef13afb6c9737321f2d44b7efbce
RealTim0 <gemtimo994@gmail.com>
2023-06-30 14:36:09 +0400

    app correction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom';
-import Table from './table';
- ReactDOM.render(<Table />,document.getElementById('root'));
+import App from './App';
+import "./index.css"
+ ReactDOM.render(<App />,document.getElementById('root'));
 
 
